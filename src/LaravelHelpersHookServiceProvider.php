d965dce4b16d827cd6c1773f1ae2266e2c56477b
usman <usman.ranglerz@gmail.com>
2019-08-25 15:11:32 +0500

    first commit

---
+++ src/LaravelHelpersHookServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace LaravelHelpersHook;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelHelpersHookServiceProvider extends ServiceProvider
+{
+    /**
+     * summary
+     */
+    public function __construct()
+    {
+     	// Add routers
+        app('router')->get('test', function () {
+            return 'Hello world!';
+        });   
+    }
+}
